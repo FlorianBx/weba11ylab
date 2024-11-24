16cdbb1be350d4a9b68dbce6a6568a61bce50c85
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-11-25 00:54:48 +0100

    feat ✨ upgrade navbar + add searchbar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,3 +1,34 @@
+const Guide = [
+  { text: 'Débuter', link: '/guide/introduction' },
+  { text: 'Principes POUR', link: '/guide/perceptible' },
+  { text: 'Navigation', link: '/guide/keyboard' },
+  { text: 'Contenu', link: '/guide/html-structure' },
+  { text: 'Tests et Outils', link: '/guide/testing-tools' },
+]
+
+const Links = [
+  { text: 'Add-ons', link: '/add-ons' },
+  { text: 'Ecosystem', link: '/ecosystem' },
+  { text: 'Export Size', link: '/export-size' },
+  { text: 'Recent Updated', link: '/functions.html#sort=updated' },
+  { text: 'Why no translations?', link: '/why-no-translations' },
+]
+
+const Learn = [
+  { text: 'Why VueUse', link: '/why-vueuse' },
+  { text: 'Why no translations?', link: '/why-no-translations' },
+]
+
+const release = [
+  // { text: 'v1.0.0', link: '/changelog#v100' },
+  // { text: 'v0.4.0', link: '/changelog#v040' },
+  // { text: 'v0.3.0', link: '/changelog#v030' },
+  // { text: 'v0.2.0', link: '/changelog#v020' },
+  { text: 'v0.01.0', link: 'https://github.com/FlorianBx/weba11ylab/releases/tag/0.010' },
+]
+
+const currentRelease = release[0].text
+
 export default {
   base: '/',
   title: 'WebA11yLab',
@@ -5,10 +36,27 @@ export default {
   head: [['link', { rel: 'icon', type: 'image/png', href: '/logo.png' }]],
   themeConfig: {
     logo: '/logo.png',
+    search: {
+      provider: 'local',
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/FlorianBx/weba11ylab' },
+      { icon: 'discord', link: 'https://discord.gg/4uMdkdht' },
+      { icon: 'bluesky', link: 'https://bsky.app/profile/florianbx.bsky.social' },
+    ],
     nav: [
-      { text: 'Accueil', link: '/' },
+      // { text: 'Accueil', link: '/' },
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Guide', items: Guide },
+          // { text: 'Learn', items: Learn },
+          // { text: 'Links', items: Links },
+        ],
+      },
       // { text: 'Guide', link: '/guide/' },
       { text: 'Ressources', link: '/ressources' },
+      { text: currentRelease, items: release, activeMatch: '/' },
     ],
     sidebar: [
       {
@@ -57,6 +105,10 @@ export default {
     editLink: {
       pattern: 'https://github.com/FlorianBx/doc-accessibility/edit/main/docs/:path',
       text: 'Modifier cette page sur GitHub',
+    },
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2024-present Florian Beaumont',
     },
   },
 }
